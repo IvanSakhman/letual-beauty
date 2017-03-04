58d339c44ae88fcdd75fe8d1caf1fdbc60b5d9eb
Ivan Sakhman <Sakhman.I@gmail.com>
2017-03-04 22:26:10 +0200

    add pictures

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,34 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'swiper';
+import 'style!css!swiper/dist/css/swiper.css';
 
 $(() => {
 	svg4everybody();
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        spaceBetween: 50,
+        slidesPerView: 4,
+        autoplay: 3000,
+        breakpoints: {
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
 });
